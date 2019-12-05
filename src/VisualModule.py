8fdea77dee09548d4a060debe015d969b37a7f82
Jorane <joranerogier@gmail.com>
2019-12-05 11:23:05 +0100

    adjusted get_coordinate: 5x faster

--- src/VisualModule.py
+++ src/VisualModule.py
@@ -6,6 +6,7 @@ import numpy as np
 import socket
 from PIL import Image
 from stopwatch import Stopwatch
+import random
 
 # %%
 class AgentEnvironment:
@@ -63,8 +64,10 @@ class AgentEnvironment:
         #    print("will be analysed")
             # only get all extra information every 'skip_frames' times
         state_image_plus = np.array(next_state).reshape(768, 768, 3)
-        stopwatch = Stopwatch()
-        stopwatch.start()
+        stopwatch_coord = Stopwatch()
+        stopwatch_coord.start()
+        '''
+        # too slow!
         diff = np.subtract(state_image_plus, state_image)
         diff[:, :, 0] = np.array((pd.DataFrame(np.abs(diff[:, :, 0])) > 120) * diff[:, :, 0] * 10)  # Our agent
         diff[:, :, 2] = np.array((pd.DataFrame(np.abs(diff[:, :, 2])) > 120) * diff[:, :, 2] * 10)  # Enemy agent
@@ -74,8 +77,49 @@ class AgentEnvironment:
         indices_e = np.where(enemy != [0])  # For each agent
         agent_coord = (int(np.median(indices[1])), int(np.median(indices[0])))
         enemy_coord = (int(np.median(indices_e[1])), int(np.median(indices_e[0])))
-        stopwatch.stop()
-        print(f"Total time to get coords: {stopwatch.duration}")
+        stopwatch_coord.stop()
+        print(f"Total time to get coords: {stopwatch_coord.duration}")
+        '''
+        ## find coordinates with help of cv2
+        colour_boundaries = [([45, 35, 120], [80, 80, 175]), # blue (actually red, but colours are flipped when image is saved)
+                            ([120, 60, 50], [170, 130, 90])] # red (actually blue, but colours are flipped when image is saved)
+        cv2.imwrite('temp.jpg', state_image_plus)
+        img = cv2.imread('temp.jpg')
+        i = 0
+        for (lower, upper) in colour_boundaries:
+            lower = np.array(lower, dtype="uint8")
+            upper = np.array(upper, dtype="uint8")
+
+            mask = cv2.inRange(img, lower, upper)
+            if i == 1:
+                (agent_x, agent_y, _, _) = cv2.boundingRect(mask)
+                agent_coord = (int(agent_x+30), int(agent_y+30))
+                #print(f"agent coordinates: {agent_x, agent_y, agent_w, agent_h}")
+                # print mask
+                #output = cv2.bitwise_and(img, img, mask = mask)
+                #cv2.imwrite(f"red_img_{random.randrange(50)}.jpg", output)
+
+            else:
+                (enemy_x, enemy_y, _, _) = cv2.boundingRect(mask)
+                #print(enemy_x, enemy_y, enemy_w, enemy_h)
+                enemy_coord = (int(enemy_x+30), int(enemy_y+30))
+                #print(f"enemy coordinates: {enemy_coord}")
+                # print mask
+                #output = cv2.bitwise_and(img, img, mask = mask)
+                #cv2.imwrite(f"blue_img_{random.randrange(50)}.jpg", output)
+
+
+            i += 1
+
+        stopwatch_coord.stop()
+        print(f"Total time to get coords: {stopwatch_coord.duration}")
+
+        # Save image with circles to check correct center of agent
+        #red_circle_img = cv2.circle(img, agent_coord, 20, (0, 0, 255), 2)
+        #blue_red_circle_img = cv2.circle(red_circle_img, enemy_coord, 20, (255, 0, 0), 2)
+        #cv2.imwrite(f"circles_img_{random.randrange(50)}.jpg", blue_red_circle_img)
+
+
         if image == True:
             final = cv2.resize(state_image_plus, (768, 768))
             # Plot to see where the agents are in the coordenades
